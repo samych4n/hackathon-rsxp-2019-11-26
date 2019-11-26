4ebe871d6e40afe00459088f1abed96c5f787aa2
Gustavo Pires Trentini <gustavo.trentini@applysystem.com.br>
2019-11-26 18:37:16 -0300

    Colunas drag in drop

---
+++ pagina/opcoes/index.js
@@ -0,0 +1,98 @@
+
+const buttons = {
+  "btnTurn": {
+    "class": "turn-btn",
+    "text": "Turn"
+  },
+  "btnStep": {
+    "class": "step-btn",
+    "text": "Step"
+  },
+  "btnLoop": {
+    "class": "loop-btn",
+    "text": "Loop"
+  }
+}
+
+var create_element = true;
+var n_event = 1;
+
+$.map(buttons, function (button, id) {
+  $("#opcoes").append(`
+    <div class="wrap ${id}-section">
+      <div id="${id}-event-0 copy" rel="${id}" class="btn ${button.class}"> ${button.text}</div>
+    </div>
+  `);
+
+  $('.btn').draggable({
+    cursor: "move",
+    containment: "document",
+    drag: function(){
+
+      if(create_element){
+        create_element = false;
+        var rel = $(this).attr('rel');
+        //add_element( rel, '<div class="btn ">tst</div>');
+      }
+    }
+  });
+});
+
+$( "#comandos").droppable({
+  drop: function( event, ui ) {
+
+    const id_obj = ui.draggable[0].attributes.rel.value;
+    
+    var obj = $.map(buttons, function (button, id) {
+      if(id == id_obj){
+        return button;
+      }
+    });
+
+    obj = obj[0];
+
+    var element =`<div class="wrap ${id_obj}-section-script">
+        <div id="${id_obj}-event-0 copy" rel="${id_obj}" class="btn ${obj.class}"> ${obj.text}</div>
+    </div>`;
+
+    add_element_script(this.id, element);
+    reset_buttons();
+    create_element = true;
+    n_event ++;
+  }
+});
+
+function add_element(target, element){
+  $("."+target+"-section").append(element);
+}
+
+function add_element_script(target, element){
+  $("#"+target).append(element);
+}
+
+function reset_buttons(){
+  $("#opcoes").html('<div class="title-column">Functions</div>');
+
+  $.map(buttons, function (button, id) {
+    $("#opcoes").append(`
+      <div class="wrap ${id}-section">
+        <div id="${id}-event-0 copy" rel="${id}" class="btn ${button.class}"> ${button.text}</div>
+      </div>
+    `);
+  
+    $('.btn').draggable({
+      cursor: "move",
+      containment: "document",
+      drag: function(){
+  
+        if(create_element){
+          create_element = false;
+          var rel = $(this).attr('rel');
+          //add_element( rel, '<div class="btn ">tst</div>');
+        }
+      }
+    });
+  });
+}
+
+
