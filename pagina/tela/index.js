d3d737ca3d800cd5e7fa8a8b6bc24e5b02781db1
Samuel Deodato Luiz Costa <samuel@firmamento.com.br>
2019-11-27 10:32:31 -0300

    tela ok

--- pagina/tela/index.js
+++ pagina/tela/index.js
@@ -2,6 +2,8 @@
 function index(board) {
     $("#play").off("click");
     $("#play").on("click", function () { $("#play").off("click"); play(); });
+    $("#reset").off("click");
+    $("#reset").on("click", function () { $("#reset").off("click"); reset(); });
     const myBoard = new Mapa(board)
     $("#container").html(myBoard.drawMap());
     personagens = Personagem.createPersonagens(board, $("#container"));
@@ -30,14 +32,22 @@ function victory() {
     console.log("victory")
     levelAtual++;
     if(levelSequence[levelAtual])
-        return refresh(levelSequence[levelAtual].level, 2000);
+        return refresh(levelSequence[levelAtual].level, 2000,1);
     else
+    {
+        levelAtual = 0;
+        return refresh(levelSequence[levelAtual].level, 2000,3);
+    }
         
 }
 
 function lose() {
     console.log("lose");
-    return refresh(levelSequence[levelAtual].level, 2000);
+    return refresh(levelSequence[levelAtual].level, 2000,2);
+}
+
+function reset() {
+    return refresh(levelSequence[levelAtual].level, 2000,0);
 }
 
 
